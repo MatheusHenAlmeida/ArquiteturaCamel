03711db5fe04a2de0377966a05652a50623f7d57
Matheus <matheushdealmeida@gmail.com>
2021-02-16 18:48:30 -0300

    Feito implementacao do template

---
+++ atendimento-system/src/main/java/com/camel/arquitetura/atendimento/system/routes/PDFRoutes.java
@@ -0,0 +1,15 @@
+package com.camel.arquitetura.atendimento.system.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PDFRoutes extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("direct:gera-pdf")
+                .to("velocity://templates/ordem.vm")
+            .log("${body}")
+        ;
+    }
+}
